1bff7553d408182507a34f71db406a67a4f804f1
miighty <rickatado@hotmail.co.uk>
2023-07-14 18:48:20 +0800

    test commit

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ const Home = () => {
       <SectionTitle
         pretitle="Nextly Benefits"
         title=" Why should you use this landing page">
-        Nextly is a free landing page & marketing website template for startups
+        Nextly is a definitely free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
         completely open-source.
       </SectionTitle>
