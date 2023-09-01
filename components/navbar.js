71d2870ace2e1224889d7ea6727dc8f84adead80
miighty <rickatado@hotmail.co.uk>
2023-09-01 18:37:33 +0800

    change navbar styling

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
   const navigation = [];
 
   return (
-    <div className="w-full">
+    <div className="w-full bg-pink-600">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
         {/* Logo  */}
         <Disclosure>
@@ -15,7 +14,7 @@ const Navbar = () => {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <span className="flex items-center space-x-2 text-2xl font-medium text-yellow-500 dark:text-gray-100">
+                  <span className="flex items-center space-x-2 text-2xl font-medium text-yellow-300">
                     <span>Wentworth Wills</span>
                   </span>
                 </Link>
@@ -25,7 +24,7 @@ const Navbar = () => {
         </Disclosure>
 
         {/* menu  */}
-        <div className="hidden   lg:flex lg:items-center">
+        <div className="hidden lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
